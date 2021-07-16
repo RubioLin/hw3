0e43099529d2853750038db041f3b757a808474a
RubioLin <capricorn1230maple@gmail.com>
2021-07-16 17:52:28 +0800

    3

---
+++ 着飾る恋/characterView.swift
@@ -0,0 +1,22 @@
+//
+//  人物介紹.swift
+//  #1
+//
+//  Created by 林俊緯 on 2021/7/8.
+//
+
+import UIKit
+
+class characterView: UIViewController {
+
+    @IBOutlet var 真柴久留美: UIButton!
+    @IBOutlet var 藤野駿: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
+    
+
